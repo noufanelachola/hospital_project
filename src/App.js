3804238f68f37026f6ff1730bfd4a1c2caf6cb47
noufanelachola <noufansadiqelachola@gmail.com>
2024-10-13 22:21:15 +0530

    first aid started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 
+import FirstAid from "./Components/FirstAid/FirstAid";
+
 function App() {
+
+  const [route,setRoute] = useState("");
+
+  //Function to change Route
+  const changeRoute = (page) => {
+    setRoute(page);
+  }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <p>Helloooo</p>
+      {route === "" && (
+        <div>
+          <h1>ABCD</h1>
+          <button onClick={() => changeRoute("firstAid")}>First Aid</button>
+        </div>
+      )}
+      {route === "firstAid" && <FirstAid changeRoute={changeRoute} />}
     </div>
   );
 }
